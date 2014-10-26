2c908802c9a999ccbc89c4d13ff3b6e805462c62
Tony <tonykopetto@gmail.com>
2014-10-26 14:36:42 -0400

    M! - initial cut

--- src/main/java/com/kopetto/sample/controller/HomeController.java
+++ src/main/java/com/kopetto/sample/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.kopetto.sample.controller;
 
+import javax.servlet.http.HttpServletRequestWrapper;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,6 +15,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+    @ModelAttribute(value="pageId")
+    public String  getPageId (HttpServletRequestWrapper request) {
+    	String requestUri = request.getRequestURI();
+    	String rc = null;
+    	
+    	if (requestUri.indexOf("account") > -1)
+    		rc = "account";
+    	else if (requestUri.indexOf("search") > -1)
+    		rc = "search";
+    	else if (requestUri.indexOf("signin") > -1)
+    		rc = "signin";
+    	else 
+    		rc = "home";
+    	
+    	return rc;
+    }
+    
+    @ModelAttribute(value="requestUri")
+    public String  getRequetUrl (HttpServletRequestWrapper request) {
+    	return request.getRequestURI();
+    }
+    
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Model uiModel) {
     	return "home";
